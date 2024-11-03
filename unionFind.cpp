e50f4dd5e1c4c5b4b68d1f3a60335580e2ddbef2
zekkrron <97668100+zekkrron@users.noreply.github.com>
2024-11-03 11:56:30 +0530

    Update unionFind.cpp

--- unionFind.cpp
+++ unionFind.cpp
@@ -1,23 +1,12 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class UnionFind{
-
-private: 
-
-	vector<int> p, siz;
-
 public: 
-
+	vector<int> p, siz;
 	UnionFind(int n){
 		siz.assign(n, 1); p.assign(n, 0);
 		iota(p.begin(), p.end(), 0);
 	}
-
 	int findSet(int i){ return (p[i] == i) ? i : p[i] = findSet(p[i]); }
-
 	bool isSameSet(int i, int j){ return findSet(i) == findSet(j); }
-
 	void unionSet(int i, int j){
 		int x = findSet(i), y = findSet(j);
 		if(siz[x] < siz[y]) swap(x, y);
@@ -27,5 +16,4 @@ public:
 			siz[y] = 0;
 		}
 	}
-
 };
